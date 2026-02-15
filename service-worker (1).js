87673f61d449091cab6b55c98795f4f5321193de
Patata182 <sevidub@gmail.com>
2026-02-15 03:25:10 +0100

    Add files via upload

---
+++ service-worker (1).js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'hasta-saturno-v1';
+const urlsToCache = [
+  './retos-pareja.html',
+  './manifest.json'
+];
+
+// Instalación del service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación del service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepción de peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
